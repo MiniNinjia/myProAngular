2c5af1989121c1678ab06398ae5ae0640a8d65af
Your Name <1373726046@qq.com>
2017-09-11 11:25:04 +0800

    雷乔------构建基础路由框架

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {from} from "rxjs/observable/from";
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+  ) { }
 
   ngOnInit() {
+
   }
 
 }
