406fb6e4095b8a2343f9e414f68398946bf26f33
jingyu <975541113@qq.com>
2017-09-12 08:53:18 +0800

    首页轮播

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import {from} from "rxjs/observable/from";
+import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
-  constructor(
-  ) { }
+
+
+  constructor() {
+  }
+
 
   ngOnInit() {
-
   }
 
 }
