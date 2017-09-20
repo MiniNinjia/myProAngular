a07f5eb801848073e5f0c9a2b5d18a5286c4a9e3
jingyu <975541113@qq.com>
2017-09-20 19:54:11 +0800

    9-20胡凯彬--完整版本11111111

--- src/app/myscroll.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import {MyscrollDirective} from './myscroll.directive';
-
-describe('MyscrollDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MyscrollDirective();
-    expect(directive).toBeTruthy();
-  });
-});
